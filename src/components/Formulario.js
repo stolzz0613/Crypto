04496b31e21d97ea6fcc153b3daf13a894b83ec1
stolzz0613 <andres0613@utp.edu.co>
2020-07-20 13:24:37 -0500

    Validacion

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
+import Error from "./Error";
 import useMoneda from "../hooks/useMoneda";
 import useCriptomoneda from "../hooks/useCriptomoneda";
 import axios from "axios";
@@ -26,6 +27,7 @@ const Boton = styled.input`
 const Formulario = () => {
 
     const [listaCripto, setListaCripto] = useState([]);
+    const [error, setError] = useState(false);
 
     const MONEDAS = [
         { codigo: "USD", nombre: "Dolar de Estados Unidos" },
@@ -45,8 +47,21 @@ const Formulario = () => {
         consultarAPI()
     }, []);
 
+
+    const cotizarMoneda = e => {
+        e.preventDefault();
+        if (moneda === "" || criptomoneda === "") {
+            setError(true);
+            return;
+        }
+        setError(false);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
             <SelectMoneda />
             <SelectCripto />
             <Boton
