ad424f31fc4c4b773d6004400bb49a9aed06e217
stolzz0613 <andres0613@utp.edu.co>
2020-07-21 13:02:41 -0500

    styled Components

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color:#FFF;
+    font-family:Arial, Helvetica, sans-serif;
+`;
+const Info = styled.p`
+    font-size:18px;
+    span{
+        font-weight:bold;
+    }
+`;
+const Precio = styled.p`
+    font-size:30px;
+`;
 
 const Cotizacion = ({ resultado }) => {
 
     if (Object.keys(resultado).length === 0) return null;
     console.log(resultado)
     return (
-        <div>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>Precio más alto del día: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>Precio más bajo del día: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variación últimas 24 horas: <span>{resultado.CHANGEPCT24HOUR * 100}%</span></Info>
+            <Info>Última Actualización: <span>{resultado.LASTUPDATE}</span></Info>
 
-        </div>
+        </ResultadoDiv>
     );
 }
 
